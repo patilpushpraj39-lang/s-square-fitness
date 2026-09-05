5a3f4164a27151c397350f3cab8e80f3d8f6259a
Pushpraj Patil <patilpushpraj39@gmail.com>
2026-09-05 16:08:42 +0530

    Set GitHub Pages router base path

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: "/s-square-fitness",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
